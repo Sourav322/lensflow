eda066fe10bc8d23b52482af4de913458e6d3172
Sourav322 <sourav3223@gmail.com>
2026-03-13 22:17:26 +0530

    Update invoices.ts

--- backend/src/routes/invoices.ts
+++ backend/src/routes/invoices.ts
@@ -1,1 +1,15 @@
+import { Router } from "express"
+import { prisma } from "../lib/prisma"
 
+const router = Router()
+
+router.get("/", async (_, res) => {
+  res.json(await prisma.invoice.findMany())
+})
+
+router.post("/", async (req, res) => {
+  const invoice = await prisma.invoice.create({ data: req.body })
+  res.json(invoice)
+})
+
+export default router
