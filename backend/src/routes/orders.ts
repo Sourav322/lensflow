01af749c9fda46fe77cf592c4a7dc10e265ce75c
Sourav322 <sourav3223@gmail.com>
2026-03-13 22:15:08 +0530

    Update orders.ts

--- backend/src/routes/orders.ts
+++ backend/src/routes/orders.ts
@@ -1,1 +1,15 @@
+import { Router } from "express"
+import { prisma } from "../lib/prisma"
 
+const router = Router()
+
+router.get("/", async (_, res) => {
+  res.json(await prisma.order.findMany())
+})
+
+router.post("/", async (req, res) => {
+  const order = await prisma.order.create({ data: req.body })
+  res.json(order)
+})
+
+export default router
