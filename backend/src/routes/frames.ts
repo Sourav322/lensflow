ff411dbb62814e4c6547aac31f937b173cca72a8
Sourav322 <sourav3223@gmail.com>
2026-03-13 22:13:39 +0530

    Update frames.ts

--- backend/src/routes/frames.ts
+++ backend/src/routes/frames.ts
@@ -1,1 +1,15 @@
+import { Router } from "express"
+import { prisma } from "../lib/prisma"
 
+const router = Router()
+
+router.get("/", async (_, res) => {
+  res.json(await prisma.frame.findMany())
+})
+
+router.post("/", async (req, res) => {
+  const frame = await prisma.frame.create({ data: req.body })
+  res.json(frame)
+})
+
+export default router
