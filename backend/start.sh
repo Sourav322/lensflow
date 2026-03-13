ab8144910b9501bf9b6b88728ec0c52f938e6da8
Sourav322 <sourav3223@gmail.com>
2026-03-13 22:08:56 +0530

    Update start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-echo "Installing dependencies..."
 npm install
 
-echo "Generating Prisma Client..."
 npx prisma generate
 
-echo "Running Prisma migrations..."
-npx prisma migrate deploy --schema=./prisma/schema.prisma
+npx prisma migrate deploy
 
-echo "Building TypeScript..."
 npm run build
 
-echo "Starting LensFlow API..."
-node dist/index.js
+npm start
