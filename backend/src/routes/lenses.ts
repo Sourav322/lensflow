4bd1d6e0b3cfb4339f96196e71becba22efdc07e
Sourav322 <sourav3223@gmail.com>
2026-03-13 22:14:11 +0530

    Update lenses.ts

--- backend/src/routes/lenses.ts
+++ backend/src/routes/lenses.ts
@@ -1,1 +1,15 @@
+import { Router } from "express"
+import { prisma } from "../lib/prisma"
 
+const router = Router()
+
+router.get("/", async (_, res) => {
+  res.json(await prisma.lens.findMany())
+})
+
+router.post("/", async (req, res) => {
+  const lens = await prisma.lens.create({ data: req.body })
+  res.json(lens)
+})
+
+export default router
