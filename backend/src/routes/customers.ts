db4f00913eb899b8ecbaa2ef5fbc58df50dd1553
Sourav322 <sourav3223@gmail.com>
2026-03-13 22:13:11 +0530

    Update customers.ts

--- backend/src/routes/customers.ts
+++ backend/src/routes/customers.ts
@@ -1,1 +1,16 @@
+import { Router } from "express"
+import { prisma } from "../lib/prisma"
 
+const router = Router()
+
+router.get("/", async (_, res) => {
+  const customers = await prisma.customer.findMany()
+  res.json(customers)
+})
+
+router.post("/", async (req, res) => {
+  const customer = await prisma.customer.create({ data: req.body })
+  res.json(customer)
+})
+
+export default router
