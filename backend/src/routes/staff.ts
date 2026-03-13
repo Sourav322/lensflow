ad5a777148e133995455ed24f4f9ebc1f610c90b
Sourav322 <sourav3223@gmail.com>
2026-03-13 22:17:01 +0530

    Update staff.ts

--- backend/src/routes/staff.ts
+++ backend/src/routes/staff.ts
@@ -1,1 +1,15 @@
+import { Router } from "express"
+import { prisma } from "../lib/prisma"
 
+const router = Router()
+
+router.get("/", async (_, res) => {
+  res.json(await prisma.staff.findMany())
+})
+
+router.post("/", async (req, res) => {
+  const staff = await prisma.staff.create({ data: req.body })
+  res.json(staff)
+})
+
+export default router
