c88395d08528c8d1a85ced3f0ddc1e3049bb821a
Sourav322 <sourav3223@gmail.com>
2026-03-13 22:16:37 +0530

    Update prescriptions.ts

--- backend/src/routes/prescriptions.ts
+++ backend/src/routes/prescriptions.ts
@@ -1,1 +1,15 @@
+import { Router } from "express"
+import { prisma } from "../lib/prisma"
 
+const router = Router()
+
+router.get("/", async (_, res) => {
+  res.json(await prisma.prescription.findMany())
+})
+
+router.post("/", async (req, res) => {
+  const prescription = await prisma.prescription.create({ data: req.body })
+  res.json(prescription)
+})
+
+export default router
