4c3fe181960ff539879dc47f1626bf37ea805a5a
Sourav322 <sourav3223@gmail.com>
2026-03-13 22:08:07 +0530

    Update seed.ts

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,1 +1,25 @@
+import { PrismaClient } from "@prisma/client"
 
+const prisma = new PrismaClient()
+
+async function main() {
+
+  await prisma.user.create({
+    data: {
+      email: "admin@lensflow.com",
+      password: "admin123",
+      role: "ADMIN"
+    }
+  })
+
+  console.log("Seed completed")
+}
+
+main()
+  .catch(e => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
